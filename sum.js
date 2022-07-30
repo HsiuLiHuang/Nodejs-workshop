6e8f1e032c0b5d240d216037b010f567b5b239ae
Lily <lilyhuang841020@gmail.com>
2022-07-30 11:57:54 +0800

    finish

---
+++ sum.js
@@ -0,0 +1,16 @@
+//sum.js
+
+function sum(n){
+  let result=0;
+  for(let i=0;i<=n;i++){
+    result+=i
+  }
+  return result;
+
+
+}
+console.log(sum(3));
+console.log(sum(5));
+console.log(sum(10));
+
+
